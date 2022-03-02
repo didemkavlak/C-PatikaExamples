fb951715d76e3e1c986c593681dba5b0a6318fe7
didem kavlak <didem@pialab.com.tr>
2022-03-02 22:47:50 +0300

    if else and try-catch example

---
+++ If-Else/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time <= 11){
+                Console.WriteLine("Günaydın");
+            }else if(time <= 18){
+                Console.WriteLine("İyi günler");
+            }else{
+                Console.WriteLine("İyi Geceler");
+            }
+
+            string sonuc = time >= 6 && time < 11 ? "Günaydın!" : time <= 18 ? "İyi günler!" : "İyi geceler!";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
---
+++ Try-Catch-Finally/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try // Hata alması muhtemel ve kontrol etmek istediğiniz kod bloğu yer alır
+            {
+                Console.WriteLine("Bir sayı giriniz:");
+                int sayi1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı:" + sayi1);
+            }
+            catch (Exception ex) // O hatayı aldığınızda yapmak istediğiniz şeyi yaparsınız.
+            {
+                Console.WriteLine("HATA: " + ex.Message.ToString());
+            
+            }
+            finally // Koşuldan bağımsız çalışmasını istediğiniz kod. // Zorunlu bir alan değildir.
+            {
+                Console.WriteLine("İşlem tamamlandı");
+            }
+           
+           // Özel exception tipleri
+
+           try
+           {
+               // int a = int.Parse(null); // ArgumentNullException hatası döner
+               //int a = int.Parse("test"); //FormatException hatası döner. Çünkü "test" string değerini int dönüştüremedi.
+               int a = int.Parse("-20000000000"); // OverflowException hatası döner. Çünkü int değerleri dışında max. min değeri dışında bir değer girdiğimizde aldığımız hata.
+           }
+           // catch (ArgumentNullException ex) // Boş değer geldiğinde bu hatanın verildiğini biliyoruz.
+           //{
+               //Console.WriteLine("Boş değer girdiniz");
+               //Console.WriteLine(ex); // Hatanın içeriğini görmüş olduk.
+           //}
+        //    catch (FormatException ex)
+        //    {
+        //        Console.WriteLine("Yanlış veri tipi girdiniz.");
+        //        Console.WriteLine(ex);
+        //    }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("Çok büyük veya çok küçük bir değer girdiniz.");
+            Console.WriteLine(ex);
+        }
+
+        }
+    }
+}
