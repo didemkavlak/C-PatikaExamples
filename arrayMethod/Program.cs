3fb7f2b3d1bf02d4321f8b939adb7637562659ad
didem kavlak <didem@pialab.com.tr>
2022-03-04 16:58:17 +0300

    array

---
+++ arrayMethod/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //sort
+           int[] sayiDizisi = {22,07,04,20,21,17,14,12};
+
+           Console.WriteLine("****Sırasız Sayı Dizisi****");
+           foreach (var sayi in sayiDizisi)
+           {
+               Console.WriteLine(sayi);
+           }
+
+           Console.WriteLine("****Sıralı Sayı Dizisi****");
+           Array.Sort(sayiDizisi); //Void fonksiyon olduğu için verilen dizi üzerinde küçükten büyüğe sıralama yapar.
+
+           foreach (var sayi in sayiDizisi)
+           {
+               Console.WriteLine(sayi);
+           }
+
+           //Clear
+           Console.WriteLine("****Array Clear****");
+            // sayiDizisi elemanları kullanılarak 2. indexten itibaren 3 tane elemanı 0'lar.
+           Array.Clear(sayiDizisi,2,3);
+
+           foreach (var sayi in sayiDizisi)
+           {
+               Console.WriteLine(sayi);
+           }
+
+           //Reverse
+           Console.WriteLine("****Array Reverse****");
+
+           Array.Reverse(sayiDizisi); // Tersine çevirme işlemi, ilk elemanı son eleman yapar.
+
+           foreach (var sayi in sayiDizisi)
+           {
+               Console.WriteLine(sayi);
+           }
+
+           //IndexOf
+           Console.WriteLine("****Array IndexOf****");
+           // Verdiğiniz elemanın eğer içinde varsa indexini döner.
+
+           Console.WriteLine(Array.IndexOf(sayiDizisi, 20));
+
+           //Resize
+           Console.WriteLine("****Array Resize****");
+           //Yeniden boyutlandırma yapar.
+           
+           Array.Resize<int>(ref sayiDizisi, 9);
+
+           sayiDizisi[8] = 3;
+
+           foreach (var sayi in sayiDizisi)
+           {
+               Console.WriteLine(sayi);
+           }
+           
+             
+        }
+    }
+}
---
+++ diziler/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizileri tanımlama
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = {"Kedin", "Köpek", "Kuş", "Fare"};
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //Dizilere değer atama ve erişme
+
+            renkler[1] = "Mavi";
+
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[3]);
+            Console.WriteLine(renkler[1]);
+            Console.WriteLine(dizi[3]);
+
+            // Döngülerde dizi kullanımı
+            // Klavyeden girilen n tane sayının ortalmasını hesaplayan program
+
+            Console.Write("Lütfen dizi uzunluğunu giriniz: ");
+            int diziUzunluğu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunluğu];
+
+            for (int i = 0; i < diziUzunluğu; i++)
+            {
+                Console.Write("Lütfen {0}. diziyi giriniz: ", i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+                
+            }
+
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            
+            Console.WriteLine("Ortalama: " + toplam/diziUzunluğu);
+
+        }
+    }
+}
