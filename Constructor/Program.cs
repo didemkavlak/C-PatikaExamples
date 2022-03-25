f14834602f5f4c6983ff99c8f6af3c2896132c52
didem kavlak <didem@pialab.com.tr>
2022-03-25 21:38:00 +0300

    Class and Constructor

---
+++ Constructor/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kurucu metotların isimleri class isimleriyle aynı olmalıdır.
+            //Erişim belirleyicileri Public olmak zorunda
+            //Geri dönüş tipleri yoktur, burada geri dönüş tipi yazmıyoruz.
+            //Kurucu metotlar default olarak var olmasını istediğimiz durumlarda kullanılır.Yeni nesle oluştuğunda otonatik olarak çalışır.
+            //Kurucu fonksiyonlar overload edilebilir.
+
+            Calisan calisan1 = new Calisan("Elif","Yılmaz", 123456, "İnsan Kaynakları");
+            calisan1.CalisanBilgileri();
+
+            Console.WriteLine("*********");
+
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Aslı";
+            calisan2.Soyad = "Deneme";
+            calisan2.No = 654321;
+            calisan2.Departman = "Muhasebe";
+            calisan2.CalisanBilgileri();
+
+            Console.WriteLine("*********");
+
+            Calisan calisan3 = new Calisan("Mehmet","Can");
+            calisan3.CalisanBilgileri();
+            //Çıktısında no sıfır ve departman boş gelir. Çünkü biz değerleri atamadan önce arkda bu popertyler için stringse null, int 0 ve boolean ise false değer atanmış durumdadır.Bunları yazıdırınca da 0 ve boş değerler bize döndü.
+            //Varsayılan Kurucu Metot
+            //Her sınıfın biz tanımlamasakta bir tane kurucu metotu vardır. Buna varsayılan kurucu metot (default constructor) denir.
+            //Default constructor'ın görevi sınıfın içerindeki özelliklere ilk değer ataması yapılmadığında onların default değerlerini set etmektir. Şöyle düşünebilirsiniz; sınıf içerisinde string veri tipinde bir özellik varsa ve siz ilk değer atamasını yapmazsanız varsayılan kurucu onun atamasını arka planda null olarak yapar. Aynı integer tipinde bir özelliğin ilk değer atamasını 0 olarak yapar.
+
+           
+        }
+    }
+
+    class Calisan{
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+         //Nesneye değer atama işlemini kurucu fonk. ile yapalım. Refactor(iyileştirmek)
+        public Calisan(string ad, string soyad, int no, string departman) //Kurucu Fonksiyon
+        {
+            //Ad = ad;
+            this.Ad = ad; //Parametre tanımlama
+            this.Soyad = soyad;
+            this.No = no;
+            this.Departman = departman;
+
+        }
+
+        //OverLoad.
+        //Önceki derste yazdıklarımız çalışabilsin diye nesne almayan aynı kurucu metodu burada yazdık.
+        public Calisan(){}
+
+        //OverLoad.
+        //Sadece ad ve soyad ile çalışan oluşturabiliyor olmalıyım.Her zaman no. ya da departman belirli olmayabilir.
+        public Calisan(string ad, string soyad){
+            this.Ad = ad;
+            this.Soyad = soyad;
+        }
+
+        public void CalisanBilgileri(){
+            Console.WriteLine("Çalışan adı: {0}", Ad);
+            Console.WriteLine("Çalışan soyadı: {0}", Soyad);
+            Console.WriteLine("Çalışan No: {0}", No);
+            Console.WriteLine("Çalışan Derpartman: {0}", Departman);
+        }
+    }
+}
---
+++ class/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                class SinifAdi : Metot ve property(özelliklerden oluşabilir.)
+                    {
+                        [Erişim Belirleyici][Veri Tipi] ÖzellikAdı; //Property
+                        [Erişim Belirleyici][Geri Dönüş Değerinin Tipi] MetotAdi([Parametreler]) //Metot
+                        {
+                            //Metot Gövdesi
+                        }
+                    }
+            */
+
+            /*
+                Erişim Belirleyiciler
+
+                Erişim belirleyiciler önüne geldiği öğenin projenin nerelerinden erişilebileceğini belirler. Öğeleri korur gibi düşünebilirsiniz.
+
+                1. Public : Her yerden erişilebilir.
+
+                2. Private : Sadece tanımlandığı sınıf içerisinden erişilebilir.
+
+                3. Internal : Sadece bulunduğu proje içerisinden erişilebilir
+
+                4. Protected : Sadece tanımlandığı sınıfta ya da o sınıfı miras alan sınıflardan erişilebilir.
+            */
+
+            //instance yaratma
+            Calisan calisan1 = new Calisan();
+
+            calisan1.Ad = "Ayşe"; //property tanımlama
+            calisan1.Soyad = "Yılmaz";
+            calisan1.No = 123456;
+            calisan1.Departman = "İnsan Kaynakları";
+
+            calisan1.CalisanBilgileri(); // Nesne aracılığı le popertylere erişilebildiği gibi metotlara da erişilebilir.
+
+            Console.WriteLine("************");
+
+            //Yeni nesne için yeni instance yaratılır
+            Calisan calisan2 = new Calisan();
+
+            calisan2.Ad = "John";
+            calisan2.Soyad = "Smith";
+            calisan2.No = 654321;
+            calisan2.Departman = "Customer Support";
+
+            calisan2.CalisanBilgileri();
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad; //Property
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri() // Method
+        {
+            Console.WriteLine("Çalışan Adı:{0}",Ad);
+            Console.WriteLine("Çalışan Soyadı:{0}",Soyad);
+            Console.WriteLine("Çalışan Numarası:{0}",No);
+            Console.WriteLine("Çalışan Depratman:{0}",Departman);
+        }
+    }
+}
